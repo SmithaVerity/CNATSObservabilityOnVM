ee7c36c1f40df50346accf74172bea0453253558
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-06-27 19:15:25 +0530

    Update installGrafana.sh

--- installGrafana.sh
+++ installGrafana.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
-sudo apt-get install grafana-enterprise
+sudo apt-get install grafana-enterprise -y
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
