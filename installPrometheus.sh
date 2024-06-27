06c505677057f9a3f99581f4a0b4d1164d5b3b8f
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-06-27 19:07:40 +0530

    Create installPrometheus.sh

---
+++ installPrometheus.sh
@@ -0,0 +1,14 @@
+sudo useradd --no-create-home --shell /bin/false prometheus
+sudo mkdir /etc/prometheus /var/lib/prometheus
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /var/lib/prometheus
+curl -LO https://github.com/prometheus/prometheus/releases/download/v2.37.5/prometheus-2.37.5.linux-amd64.tar.gz
+tar -xvf prometheus-2.37.5.linux-amd64.tar.gz
+sudo cp -p ./prometheus-2.37.5.linux-amd64/prometheus /usr/local/bin
+sudo chown prometheus:prometheus /usr/local/bin/prom*
+sudo cp -r ./prometheus-2.37.5.linux-amd64/consoles /etc/prometheus
+sudo cp -r ./prometheus-2.37.5.linux-amd64/console_libraries /etc/prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo cp -p ./prometheus-2.37.5.linux-amd64/prometheus.yml /etc/prometheus
+sudo nano /etc/prometheus/prometheus.yml
